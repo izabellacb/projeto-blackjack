657a2141324db893f0a18bd4a5343c4da5053a87
izabellacb <izabellacb.icb@gmail.com>
2023-03-21 12:13:25 -0300

    adiciona parte I do projeto

--- nossoJogo.js
+++ nossoJogo.js
@@ -10,3 +10,72 @@
  * 
  * 
  */
+
+console.log(`Boas Vindas ao jogo de BlackJack!`)
+const iniciarRodada = confirm(`Quer iniciar uma nova rodada?`)
+
+const cartaUsuario = comprarCarta() 
+const cartaComputador = comprarCarta() 
+const pontuacaoUser = cartaUsuario.valor + cartaUsuario.valor
+const pontuacaoComputer = cartaComputador.valor + cartaComputador.valor
+
+/*
+if(iniciarRodada === false)
+   console.log(`O jogo acabou`)
+else{
+
+   if(pontuacaoUser > pontuacaoComputer){
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser}\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer}\nO usuário ganhou!`)
+            
+      
+   }else if(pontuacaoUser < pontuacaoComputer){
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser}\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer}\nO computador ganhou!`)
+   
+   }else if(pontuacaoUser === pontuacaoComputer){
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser}\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer}\nEmpate!!!`)
+      
+      
+   } 
+
+}
+*/
+
+if(iniciarRodada === false)
+   console.log(`O jogo acabou`)
+else {
+
+   if(pontuacaoUser > 21){
+    console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser} Pontuação maior que 21. Jogador perdeu!\n`,
+    `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer} Vencedor!!!`)
+   } else {
+
+   if(pontuacaoComputer > 21){
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser} Vencedor!!!\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer} Pontuação maior que 21. Jogador perdeu!`)
+   } else {
+
+   if(pontuacaoUser > pontuacaoComputer) {
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser}\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer}\nO usuário ganhou!`)
+
+   }else if(pontuacaoUser < pontuacaoComputer){
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser}\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer}\nO computador ganhou!`)
+
+   }else if(pontuacaoUser === pontuacaoComputer){
+      console.log(`Usuário - cartas: ${cartaUsuario.texto} ${cartaUsuario.texto} - pontuação ${pontuacaoUser}\n`,
+      `Computador - cartas: ${cartaComputador.texto} ${cartaComputador.texto} - pontuação ${pontuacaoComputer}\nEmpate!!!`)
+   }
+   }  
+   }
+
+}
+
+
+
+
+
+
